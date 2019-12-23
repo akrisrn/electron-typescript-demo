69af7ed3bdff2649dd89a35f45b5847a5e6d7fea
AkrISrn <shairkarine@gmail.com>
2019-12-23 12:23:38 +0800

    small fix

--- src/main.ts
+++ src/main.ts
@@ -18,13 +18,13 @@ const createWindow = () => {
 };
 
 app.on("ready", createWindow);
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
-});
 app.on("activate", () => {
     if (mainWindow === null) {
         createWindow();
+    }
+});
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
     }
 });
